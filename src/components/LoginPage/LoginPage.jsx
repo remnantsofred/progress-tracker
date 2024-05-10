b236aaa208e4c2cc778a68717b83ff35129b18a7
remnantsofred <remnantsofred@gmail.com>
2024-05-10 15:29:51 -0700

    Clean up login page code

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -6,74 +6,65 @@ import { useState } from 'react';
 
 
 export const LoginPage = () => {
-  // const [signedIn, setSignedIn] = useState(false);
   const [signInHidden, setSignInHidden] = useState(true);
   const [signOutHidden, setSignOutHidden] = useState(false);
   const [loggedinUser, setLoggedinUser] = useState('');
+  // initialie firebase
   startFirebase();
+  
   // set up auth
-  // const signInBtn = document.getElementById('signInBtn');
-  // const signOutBtn = document.getElementById('signOutBtn');
-
   const auth = firebase.auth();
   const provider = new firebase.auth.GoogleAuthProvider();
 
-  /// Sign in event handlers
-
-  // signInBtn.onclick = () => auth.signInWithPopup(provider);
-  // signOutBtn.onclick = () => auth.signOut();
-
-  // const whenSignedOut = document.getElementById('whenSignedOut');
-  // const userDetails = document.getElementById('userDetails');
-
-
+  // Sign in state change
   auth.onAuthStateChanged(user => {
       if (user) {
           // signed in
-          // whenSignedIn.hidden = false;
           setSignInHidden(false);
           setSignOutHidden(true);
-          // whenSignedOut.hidden = true;
-          setLoggedinUser(user)
-
-
-          // userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+          setLoggedinUser(user);
       } else {
           // not signed in
-          // whenSignedIn.hidden = true;
           setSignInHidden(true);
           setSignOutHidden(false);
-          // whenSignedOut.hidden = false;
           setLoggedinUser('')
-          // userDetails.innerHTML = '';
       }
   });
 
 
   return (
     <div className='main-page'>
-      <section id="whenSignedOut" hidden={signOutHidden}>
+      <section 
+        id="whenSignedOut" 
+        hidden={signOutHidden}>
 
-      <h1>Login</h1>
-      <button 
-        id="signInBtn"
-        onClick={() => auth.signInWithPopup(provider)}
-        >
-        Sign in with Google</button>
+        <h1>Login</h1>
+        <button 
+          id="signInBtn"
+          onClick={() => auth.signInWithPopup(provider)}
+          >
+          Sign in with Google
+        </button>
   
       </section>
     
-      <section id="whenSignedIn" hidden={signInHidden}>
+      <section 
+        id="whenSignedIn" 
+        hidden={signInHidden}>
+
         <h1>Welcome</h1>
-        <div id="userDetails">{loggedinUser ? `${loggedinUser.displayName} - ${loggedinUser.uid}` : ''}</div>
+        <div 
+          id="userDetails">
+            {loggedinUser ? `${loggedinUser.displayName} - ${loggedinUser.uid}` : ''}
+        </div>
         <button 
           id="signOutBtn"
           onClick={() => auth.signOut()}
           >
-          Sign Out</button>
+          Sign Out
+        </button>
     
       </section>
-
     </div>
   )
 }
