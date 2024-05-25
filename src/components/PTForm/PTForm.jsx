bb19459f38db26d834b44952a0ee3ff5171987df
remnantsofred <remnantsofred@gmail.com>
2024-05-24 19:22:51 -0700

    Create PT form

--- src/components/PTForm/PTForm.jsx
+++ src/components/PTForm/PTForm.jsx
@@ -3,13 +3,30 @@ import TextField from '@mui/material/TextField';
 import AddIcon from '@mui/icons-material/Add';
 import PTFormInputRow from './PTFormInput';
 import Stack from '@mui/material/Stack'
-import { useState } from 'react';
+import { useState, useContext} from 'react';
 import Button from '@mui/material/Button';
 import PTFormItemRow from './PTFormItemRow';
 import Divider from '@mui/material/Divider';
+import { AuthContext } from '../../AuthContext';
+import { doc, setDoc, collection } from "firebase/firestore";
+import { db } from '../../../firebase';
+
 
 const PTForm = () => {
   const [planRows, setPlanRows] = useState([]);
+  const currentUser = useContext(AuthContext);
+
+  const createPlan = () => {
+    const ptRef = doc(collection(db, 'pt-plans'));
+    const newPTPlans = [];
+    for (let plan of planRows) {
+      const planObj = {'name': plan[0],'goal': plan[1], 'uid': currentUser.uid}
+      newPTPlans.push(planObj)
+    }
+    setDoc(ptRef, {'uid': currentUser.uid, 'pt-plan': newPTPlans})
+    setPlanRows([]);
+    // to do: add error handling if no name or date
+  }
 
   return (
     <form className='PT-form'>
@@ -47,7 +64,7 @@ const PTForm = () => {
           >
 
         </PTFormInputRow>
-        <Button  >Save</Button>
+        <Button  onClick={createPlan}>Save</Button>
       </Stack>
     </form>
   )
