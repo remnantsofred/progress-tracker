4d98e83bea4c9bd8957a35b8965dc39aa726895f
remnantsofred <remnantsofred@gmail.com>
2024-05-29 16:50:23 -0700

    Edit milestone modal styling

--- src/components/EditMilestoneModal/EditMilestoneModal.jsx
+++ src/components/EditMilestoneModal/EditMilestoneModal.jsx
@@ -9,7 +9,8 @@ import CloseIcon from '@mui/icons-material/Close';
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
-
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function EditMilestoneModal({milestone, setEditModal}) {
   const [name, setName] = useState(milestone.data().name);
@@ -24,58 +25,24 @@ export default function EditMilestoneModal({milestone, setEditModal}) {
   }
 
   return (
-    <div className='edit-milestone-modal'>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
-            m: 1,
-            minWidth: 300,
-            height: 300,
-          },
-        }}
-      >
-        <Paper elevation={3} >
-          <div action="" className="div">
-          <div className='row'>
-            <h3>Edit milestone</h3>
-            <IconButton
-              size="small"
-              aria-label="close"
-              color="inherit"
-              onClick={() => setEditModal('')}
-            >
-              <CloseIcon fontSize="small" />
-            </IconButton>
-          </div>
-          <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField id="outlined-basic" name="name" label="Name" variant="outlined"onChange={ (e) => setName(e.target.value)} value={name}/>
-          </Box>
-          {/* </div> */}
-          <Box 
-            component="form"
-            // sx={{
-            //   '& > :not(style)': { m: 1, width: '25ch' },
-            // }}
-            // noValidate
-            
-            >
-            {/* <BasicDateTimePicker></BasicDateTimePicker> */}
-            <input type="datetime-local" name="date" id="date" onChange={ (e) => setDate(e.target.value)} value={date}/>
-          </Box>
-          <div onClick={updateMilestone}>Save</div>
-        </div>
-        </Paper>
-      </Box>
-
-    </div>
+    <Paper elevation={3} className='edit-milestone-modal'>
+      <Stack direction='row'>
+        <h3>Edit milestone</h3>
+        <IconButton
+          size="small"
+          aria-label="close"
+          color="inherit"
+          onClick={() => setEditModal('')}
+          id="edit-milestone-close-button"
+        >
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      </Stack>
+      <Stack spacing={2.5} >
+        <TextField id="outlined-basic" name="name" label="Name" variant="outlined"onChange={ (e) => setName(e.target.value)} value={name}/>
+        <input type="datetime-local" name="date" id="date" onChange={ (e) => setDate(e.target.value)} value={date}/>
+        <Button onClick={updateMilestone}>Save</Button>
+      </Stack>
+    </Paper>
   );
 }
