bc6032ebeaac81fa0cb91e05ccfba37c6e562803
remnantsofred <remnantsofred@gmail.com>
2024-05-31 15:10:01 -0700

    Swtich to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import './App.css'
 import {  useState } from 'react';
-import { AuthContext } from './AuthContext';
-import { LoginPage } from './components/LoginPage/LoginPage'
-import { MilestonesPage } from './components/MilestonesPage/MilestonesPage.jsx'
-import ButtonAppBar from './components/AppBar/AppBar';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { AuthContext } from './AuthContext.jsx';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { MilestonesPage } from './components/MilestonesPage/MilestonesPage.jsx';
+import BasicMenu from './components/BasicMenu/BasicMenu.jsx';
+import EditMilestoneModal from './components/EditMilestoneModal/EditMilestoneModal.jsx';
 import MilestoneForm from './components/MilestoneForm/MilestoneForm.jsx';
+import PTPage from './components/PTPage/PTPage.jsx';
 
 function App() {
   const [currentUser, setCurrentUser] = useState('');
+  const [editModal, setEditModal] = useState('');
   
   return (
     <>
-      {/* <AuthContext.Provider value={currentUser}>
-        <ButtonAppBar></ButtonAppBar>
-        <h1>Progress Tracker</h1>
-        <LoginPage currentUser={currentUser} setCurrentUser={setCurrentUser}/>
-        { currentUser && <MilestonesPage />}
-        { currentUser && <MilestoneForm />}
-      </AuthContext.Provider > */}
+      <AuthContext.Provider value={currentUser}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <BasicMenu currentUser={currentUser} setCurrentUser={setCurrentUser}></BasicMenu>
+          <h1>Progress Tracker</h1>
+          { editModal !== '' && <EditMilestoneModal milestone={editModal} setEditModal={setEditModal} ></EditMilestoneModal>}
+          { currentUser && <PTPage />}
+          { currentUser && <MilestonesPage setEditModal={setEditModal}/>}
+          { currentUser && <MilestoneForm />}
+          {/* <div id="detail">
+            {currentUser && <Outlet milestone={editModal} setEditModal={setEditModal} />}
+          </div> */}
+        </LocalizationProvider>
+      </AuthContext.Provider >
     </>
   )
 }
 
-export default App
+export default App;
